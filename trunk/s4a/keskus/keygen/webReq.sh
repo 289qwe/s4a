6dc71b685346fbda4b6c104d431c6a0db4a1c7b8
matu550 <matu550@2e9b2f36-8dcd-2c5b-f6d0-f78de1e01296>
2010-04-09 13:50:16 +0000

    S4A-Centre 4.6.2 update

--- trunk/s4a/keskus/keygen/webReq.sh
+++ trunk/s4a/keskus/keygen/webReq.sh
@@ -2,7 +2,8 @@
 
 # Copyright (C) 2010, Cybernetica AS, http://www.cybernetica.eu/
 
-
+CERTDIR="/etc/ssl"
+PRIVDIR="$CERTDIR/private"
 set -e
 
 if [ "$1" = "" ]
@@ -20,11 +21,18 @@ DISNAME="/C=EE/CN=$SUBJEKT"
 NEWKEY="confservkey.key"
 NEWREQ="confservkey.req"
 
+if [ -s $NEWREQ ]
+then
+	mv $NEWREQ $NEWREQ.`date '+%s'`
+fi
+
 openssl req -sha512 -new -keyout $NEWKEY -out $NEWREQ -nodes -subj "$DISNAME" -newkey rsa:2048
 
 # panen failid kuhu vaja
-cp $NEWKEY $NEWREQ /var/www/conf/certs/
+if [ -s $PRIVDIR/$NEWKEY ]
+then
+	mv $PRIVDIR/$NEWKEY $PRIVDIR/$NEWKEY.`date '+%s'`
+fi
+mv $NEWKEY $PRIVDIR/$NEWKEY
 
 echo "\nPlease transmit the file \"$NEWREQ\" to the CA administrator for certify"
-
-exit 0
